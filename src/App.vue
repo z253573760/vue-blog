951ba6d485f8787691f65bd655cda3d775f93b1b
SC-201711192035\Administrator <253573760@qq.com>
2018-09-19 14:18:37 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,8 @@ export default {
   },
   watch: {
     menuShow: {
-      handler(cur) {
-        console.log(cur);
+      handler(cur, old) {
+        console.log(cur, old);
         this.isShow = cur;
       }
     }
