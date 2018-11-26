fc06b7dc11c617b66613b713deb3d8ef54eeba95
zhoucanyu <253573760@qq.com>
2018-11-26 18:08:17 +0800

    no message

---
+++ src/views/Zone/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="menu">
+      <tree-menu />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "treeMenu",
+  props: ["menu"],
+  computed: {
+    isMenu() {
+      const is = Array.isArray(this.menu);
+      return is;
+    }
+  },
+  created() {
+    console.log(this.menu);
+  }
+};
+</script>
+
