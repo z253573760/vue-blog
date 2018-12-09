ca7fb18e98e6104ef1c32387b99b7a861bc6e43a
SC-201711192035\Administrator <253573760@qq.com>
2018-12-10 00:44:54 +0800

    no message

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,22 +1,23 @@
 <template>
-  <div class="home"
-       @touchmove.prevent="()=>{}">
-    <vue-particles color="#fff"
-                   :particleOpacity="0.7"
-                   :particlesNumber="60"
-                   shapeType="circle"
-                   :particleSize="4"
-                   linesColor="#fff"
-                   :linesWidth="1"
-                   :lineLinked="true"
-                   :lineOpacity="0.4"
-                   :linesDistance="150"
-                   :moveSpeed="2"
-                   :hoverEffect="true"
-                   hoverMode="grab"
-                   :clickEffect="true"
-                   clickMode="push"
-                   class="lizi">
+  <div class="home" @touchmove.prevent="() => {}">
+    <vue-particles
+      color="#fff"
+      :particleOpacity="0.7"
+      :particlesNumber="60"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#fff"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="2"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+      class="lizi"
+    >
     </vue-particles>
     <Card />
   </div>
