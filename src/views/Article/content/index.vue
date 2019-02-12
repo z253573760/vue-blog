7c3396427bc3e9783b0a95c0e59db86d710d7bef
SC-201711192035\Administrator <253573760@qq.com>
2019-02-12 20:08:21 +0800

    增加标签

--- src/views/Article/content/index.vue
+++ src/views/Article/content/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="content">
     <h1 class="title">{{title}}</h1>
+    <div class="skill">
+      <div v-for="item in skills"
+           :key="item.id">
+        {{item.title}}
+      </div>
+    </div>
     <p class="time">
       <span>{{updateTime|fmtDate}}</span>
     </p>
@@ -23,6 +29,7 @@ export default {
     return {
       title: "",
       content: "",
+      skills: [],
       updateTime: new Date(),
       createTime: new Date()
       // toolbars
@@ -35,13 +42,15 @@ export default {
           title,
           content,
           create_time: createTime,
-          update_time: updateTime
+          update_time: updateTime,
+          skills
         }
       } = await getArticle(this.id);
       this.title = title;
       this.content = content;
       this.createTime = createTime;
       this.updateTime = updateTime;
+      this.skills = skills;
     }
   },
   computed: {
@@ -56,12 +65,24 @@ export default {
   background: white;
   padding: 15px;
   .title {
-    font-size: 26px;
+    font-size: 32px;
     text-align: center;
     margin-bottom: 15px;
   }
+  .skill {
+    display: flex;
+    font-size: 24px;
+    div {
+      padding: 5px 10px;
+      margin-right: 10px;
+      background: rgb(129, 138, 145);
+      color: #fff;
+      border-radius: 5px;
+    }
+  }
   .time {
     text-align: right;
+    font-size: 20px;
   }
   .article {
     overflow-x: hidden;
